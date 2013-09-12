aab8847abdcddf77212813aa717481e0d11ef31c
John Hiott <jhiott@oolcay.com>
2013-09-11 21:59:47 -0400

    added option to create config file only

--- README.md
+++ README.md
@@ -9,7 +9,14 @@ vhost.sh
 * Creates /etc/apache2/sites-available/mydomain.com
 * Creates host directory: /var/www/mydomain.com/public
 
+For Creating host directory and config
 ```sh
 ./vhost.sh mydomain.com
 ```
+
+To create config file only
+```
+./vhost.sh mydomain.com off
+```
+
 For now this scripts only allows one parameter: domain name
--- vhost.sh
+++ vhost.sh
@@ -1,64 +1,88 @@
-	# Make sure there is 1 command line argument
-	if [[ $# != 1 ]]; then
-    	die
-	fi
-
 	SERVER_DOMAIN=$1
 
-	echo "Adding Virtual Host" $SERVER_DOMAIN
+	function createWWW () {
+		echo "creating directory /var/www/"$SERVER_DOMAIN"/public"
+		sudo mkdir /var/www/$SERVER_DOMAIN
+		sudo mkdir /var/www/$SERVER_DOMAIN/public
+		sudo chmod -R 777 /var/www/$SERVER_DOMAIN
+		echo "It Works" > /var/www/$SERVER_DOMAIN/public/index.html
+	}
 
-	echo "creating directory /var/www/"$SERVER_DOMAIN"/public"
-	sudo mkdir /var/www/$1
-	sudo mkdir /var/www/$1/public
-	sudo chmod -R 777 /var/www/$1
-	echo "It Works" > /var/www/$1/public/index.html
+	function createConfig () {
+		echo "Adding Virtual Host" $SERVER_DOMAIN
+		sudo sh -c 'echo "<VirtualHost *:80>
+			ServerAdmin webmaster@localhost
+			ServerName '${SERVER_DOMAIN}'
+			ServerAlias www.'${SERVER_DOMAIN}'
+			
+			DocumentRoot /var/www/'${SERVER_DOMAIN}'/public
+			<Directory />
+				Options FollowSymLinks
+				AllowOverride None
+			</Directory>
+			<Directory /var/www/>
+				Options Indexes FollowSymLinks MultiViews
+				AllowOverride None
+				Order allow,deny
+				allow from all
+			</Directory>
 
-	sudo sh -c 'echo "<VirtualHost *:80>
-	ServerAdmin webmaster@localhost
-	ServerName '${SERVER_DOMAIN}'
-	ServerAlias www.'${SERVER_DOMAIN}'
+			ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
+			<Directory "/usr/lib/cgi-bin">
+				AllowOverride None
+				Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
+				Order allow,deny
+				Allow from all
+			</Directory>
 
-	DocumentRoot /var/www/'${SERVER_DOMAIN}'/public
-	<Directory />
-		Options FollowSymLinks
-		AllowOverride None
-	</Directory>
-	<Directory /var/www/>
-		Options Indexes FollowSymLinks MultiViews
-		AllowOverride None
-		Order allow,deny
-		allow from all
-	</Directory>
+			ErrorLog ${APACHE_LOG_DIR}/error.log
 
-	ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
-	<Directory "/usr/lib/cgi-bin">
-		AllowOverride None
-		Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
-		Order allow,deny
-		Allow from all
-	</Directory>
+			# Possible values include: debug, info, notice, warn, error, crit,
+			# alert, emerg.
+			LogLevel warn
 
-	ErrorLog ${APACHE_LOG_DIR}/error.log
+			CustomLog ${APACHE_LOG_DIR}/access.log combined
+    		Alias /doc/ "/usr/share/doc/"
+    		<Directory "/usr/share/doc/">
+        		Options Indexes MultiViews FollowSymLinks
+        		AllowOverride None
+        		Order deny,allow
+        		Deny from all
+        		Allow from 127.0.0.0/255.0.0.0 ::1/128
+    		</Directory>
+		</VirtualHost>" > /etc/apache2/sites-available/'${SERVER_DOMAIN}
+	}
 
-	# Possible values include: debug, info, notice, warn, error, crit,
-	# alert, emerg.
-	LogLevel warn
+	function showUsage() {
+		echo "To create config and www directory: ./vhost.sh mydomain.com"
+    	echo "To create config only: ./vhost.sh mydomain.com off"
+		exit
+	}
 
-	CustomLog ${APACHE_LOG_DIR}/access.log combined
+	function restartApache() {
+		echo "Restarting Apache"
+		sudo service apache2 restart
+		exit
+	}
 
-    Alias /doc/ "/usr/share/doc/"
-    <Directory "/usr/share/doc/">
-        Options Indexes MultiViews FollowSymLinks
-        AllowOverride None
-        Order deny,allow
-        Deny from all
-        Allow from 127.0.0.0/255.0.0.0 ::1/128
-    </Directory>
+		
+	#if there in only domain in arguements
+	if [[ $# == 1 ]]; then
+    	createWWW
+    	createConfig
+    	restartApache
+ 	fi
 
-</VirtualHost>" > /etc/apache2/sites-available/'${SERVER_DOMAIN}
+	#check second command line arguement
+	if [[ $# == 2 ]]; then
+		if [[ $2 == "off" ]]; then
+			createConfig
+			restartApache
+		fi
+	fi
 
-	echo "Restarting Apache"
-	sudo service apache2 restart
+	showUsage
+	
 
 
 
